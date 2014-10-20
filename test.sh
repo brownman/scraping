49190e1422533247d30a2ab487dabde4264b4664
brownman <brownman556@gmail.com>
2014-10-20 11:49:45 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 run(){
+local file cmd
 while read line;do
-  cmd='casperjs BANK/$line'
+  file=BANK/$line
+  test -f $file 
+  cmd="casperjs $file"
   echo "[CMD] $cmd"
   eval "$cmd"
 done < <( ls -1 BANK/ )
