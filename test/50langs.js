50de8c538ce23ebb58bc6ac936b731ce024e1c74
brownman <brownman556@gmail.com>
2014-11-21 14:00:57 +0200

    Update 50langs.js

--- test/50langs.js
+++ test/50langs.js
@@ -1,4 +1,15 @@
 // googletesting.js
+//https://travis-ci.org/n1k0/casperjs/builds
+
+function getLinks() {
+    var links = document.querySelectorAll('.style2 , td.Stil39:nth-child(1)');
+     
+    return Array.prototype.map.call(links, function(e) {
+               return e.text;
+//        return e.getAttribute('href');
+    });
+    
+}
 casper.test.begin('page should contains 100 lessons', 1, function suite(test) {
     casper.start("http://www.goethe-verlag.com/book2/EM/EMIT/EMIT002.HTM", function() {
         test.assertTitle("English US - Italian for beginners – Table of contents", "title is: langX -> langY");
@@ -12,11 +23,17 @@ casper.test.begin('page should contains 100 lessons', 1, function suite(test) {
     casper.then(function() {
        // test.assertTitle("casperjs - Recherche Google", "google title is ok");
     //    test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll(".style2 , td.Stil39:nth-child(1)").length = 100;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
+      /*  test.assertEval(function() {
+            return __utils__.findAll(".style2 , td.Stil39:nth-child(1)").length = [];
+        }, "prepare an array of all lessons");
+        
     });
+    */
+var arr1 = [];
+var arr2 = getLinks();
+//documnt.get(".style2 , td.Stil39:nth-child(1)")
 
+casper.test.assertEquals(arr1, arr2)
     casper.run(function() {
         test.done();
     });
