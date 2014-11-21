c46021fd73c0b94890ccaa10f806147cca839f04
brownman <brownman556@gmail.com>
2014-11-21 12:55:24 +0200

    Update 50langs.js

--- test/50langs.js
+++ test/50langs.js
@@ -13,7 +13,7 @@ casper.test.begin('page should contains 100 lessons', 1, function suite(test) {
        // test.assertTitle("casperjs - Recherche Google", "google title is ok");
     //    test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
         test.assertEval(function() {
-            return __utils__.findAll(".style2 , td.Stil39:nth-child(1)").length >= 10;
+            return __utils__.findAll(".style2 , td.Stil39:nth-child(1)").length = 100;
         }, "google search for \"casperjs\" retrieves 10 or more results");
     });
 
