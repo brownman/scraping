cc708069a5679fa9ee44c8ed2e5b2f805549946d
brownman <brownman556@gmail.com>
2014-11-22 14:21:48 +0200

    Update test.js

--- phantom/test.js
+++ phantom/test.js
@@ -1,10 +1,13 @@
 // Read the Phantom webpage '#intro' element text using jQuery and "includeJs"
 
+var selector='.Stil39 a'
+var url='http://www.goethe-verlag.com/book2/EM/EMIT/EMIT002.HTM'
 var page = require('webpage').create();
 
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
+
 
 page.open("http://www.phantomjs.org", function(status) {
     if ( status === "success" ) {
@@ -15,4 +18,15 @@ page.open("http://www.phantomjs.org", function(status) {
             phantom.exit();
         });
     }
+});
+
+var page = require('webpage').create();
+page.open(url, function() {
+  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+    page.evaluate(function() {
+      var something=$(selector).text;
+      console.log(something);
+    });
+    phantom.exit()
+  });
 });
