2f398d07ddc0922a3b43ff0e6c7d32518d0675b5
brownman <brownman556@gmail.com>
2014-11-22 15:32:47 +0200

    Update test.js

--- phantom/test.js
+++ phantom/test.js
@@ -1,6 +1,6 @@
 // Read the Phantom webpage '#intro' element text using jQuery and "includeJs"
 
-
+var url1='http://www.goethe-verlag.com/book2/EM/EMIT/EMIT002.HTM';
 var page = require('webpage').create();
 
 
@@ -8,20 +8,18 @@ page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
 
-
-
- 
-var url1='http://www.goethe-verlag.com/book2/EM/EMIT/EMIT002.HTM';
- page.open(url1, function(status) {
-     
-    if ( status === "success" ) {       
-      
-                  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-                                page.evaluate(function() {
+page.open(url1, function(status) 
+{
+    if ( status === "success" ) 
+    {       
+                  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() 
+                  {
+                                page.evaluate(function()
+                                {
                                     console.log(page.content);
                                 var selector1='.Stil39 > a';
                             
-                                  var res=$(selector1).text;
+                                  var res=$(selector1).text();
                                   console.log(res);
                                 });
                                // phantom.exit()
