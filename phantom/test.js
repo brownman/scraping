60da2090dc3f962b4e01846abbf81f8042155a4c
brownman <brownman556@gmail.com>
2014-11-22 15:13:32 +0200

    Update test.js

--- phantom/test.js
+++ phantom/test.js
@@ -18,16 +18,16 @@ page.open(url1, function(status) {
      
     if ( status === "success" ) {       
       
-  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-    page.evaluate(function() {
-        console.log(page.content);
-    var selector1='.Stil39 > a';
-
-      var res=$(selector1).text;
-      console.log(res);
-    });
-   // phantom.exit()
-  });
+                  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+                                page.evaluate(function() {
+                                    console.log(page.content);
+                                var selector1='.Stil39 > a';
+                            
+                                  var res=$(selector1).text;
+                                  console.log(res);
+                                });
+                               // phantom.exit()
+                  });
       };
 });
 
