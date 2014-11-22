7c8170f0bfa6a42a08c1a51f02f41c60fe1048ad
brownman <brownman556@gmail.com>
2014-11-22 14:46:17 +0200

    Update test.js

--- phantom/test.js
+++ phantom/test.js
@@ -23,12 +23,13 @@ page.open("http://www.phantomjs.org", function(status) {
 
 var page = require('webpage').create();
 
-var selector1='.Stil39 a';
 var url1='http://www.goethe-verlag.com/book2/EM/EMIT/EMIT002.HTM';
 var page = require('webpage').create();
 page.open(url1, function() {
   page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
     page.evaluate(function() {
+    var selector1='.Stil39 a';
+
       var res=$(selector1).text();
       console.log(res);
     });
